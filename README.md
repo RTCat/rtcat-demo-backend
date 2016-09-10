239b8fc82ae4c68ef1238637e2490f3c6bfd782e
Yi Kai <beliefs111@gmail.com>
2016-09-10 14:25:00 +0800

    release 0.2.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 实时猫Demo项目后台
+# [RTCat/rtcat-demo-web](https://github.com/RTCat/rtcat-demo-web) 后台
 
-用于实时猫Web Demo和Android 1v1 Demo的后端项目, 提供创建Session, 获取Session下token的功能
+[RTCat/rtcat-demo-web](https://github.com/RTCat/rtcat-demo-web)的后端项目, 提供创建Session, 获取Session下token的功能
 
 ## 先决条件
 
@@ -21,19 +21,6 @@
 4. 运行`npm start`命令, 运行本项目, 本项目的默认端口为8080, 如果需要运行在另外的端口, 可以通过
 `PORT=8000 npm start`的方式来调整端口
 
-5. 通过浏览器访问 http://localhost:8080/sessions , 你应该可以看到一个包含session信息的JSON数据, 形如:
-```JSON
-{
-    "uuid": "740a243d-c929-47a7-92f6-10d12bf9c425",
-    "label": "demo",
-    "type": "p2p",
-    "permanent": false,
-    "data": null,
-    "time_created": "2016-03-04T05:21:53.445828Z",
-    "live_days": 30,
-    "expire_in": "2016-04-03T05:21:53.445Z"
-}
-```
 阅读下面章节了解更多有关接口的信息.
 
 ## 接口说明
@@ -41,45 +28,12 @@
 本项目提供以下接口
 
 ```
-GET /sessions  
- 
-创建一个session, 返回新创建的session信息, 形如
-{
-    "uuid": "740a243d-c929-47a7-92f6-10d12bf9c425",
-    "label": "demo",
-    "type": "p2p",
-    "permanent": false,
-    "data": null,
-    "time_created": "2016-03-04T05:21:53.445828Z",
-    "live_days": 30,
-    "expire_in": "2016-04-03T05:21:53.445Z"
-}
-其中uuid即为session id.
+GET /tokens/:sessionName?number={number}&type={type}&
 ```
-
-```
-GET /:sessionId/tokens
-
-将sessionId替换为获取的session id, 在该session id创建一个token, 返回新创建的token信息, 形如
-{
-    "uuid": "65535df7-8e39-48f4-8d78-6413e348e9f1",
-    "session_id": "740a243d-c929-47a7-92f6-10d12bf9c425",
-    "label": null,
-    "type": "pub",
-    "permanent": false,
-    "data": null,
-    "time_created": "2016-03-04T05:43:21.407865Z",
-    "live_days": 15,
-    "expire_in": "2016-03-19T05:43:21.407Z"
-}
-其中uuid即为token
-```
-
-```
-GET /tokens/:sessionName
 
 将sessionName替换为任意名称, 如果不存在该名称的session则新建一个该名称的session,并在该session下创建一个token,
 如果存在该名称的session , 则直接在该session下创建一个token, 返回新创建的token信息, 形如:
+```
 {
     "uuid": "65535df7-8e39-48f4-8d78-6413e348e9f1",
     "session_id": "740a243d-c929-47a7-92f6-10d12bf9c425",
@@ -91,7 +45,9 @@ GET /tokens/:sessionName
     "live_days": 15,
     "expire_in": "2016-03-19T05:43:21.407Z"
 }
-其中uuid即为token, 推荐直接使用此接口
 ```
+其中uuid即为token.
+
+还可以通过querystring的形式传入创建token的其他参数
 
 
--- server.js
+++ server.js
@@ -2,12 +2,7 @@
 
 var restify = require('restify');
 var server = restify.createServer();
-var socketio = require('socket.io');
-var io = socketio.listen(server.server);
 var port = normalizePort(process.env.PORT || '8080');
-var uuid = require('node-uuid');
-var EventEmitter = require('events');
-var util = require('util');
 
 var RTCat = require('realtimecat-node-sdk');
 var config = require('./config.json');
@@ -19,138 +14,69 @@ server.use(restify.bodyParser());
 restify.CORS.ALLOW_HEADERS.push('Access-Control-Allow-Origin');
 server.use(restify.CORS());
 
-server.get('/tokens/:sessionName', function (req, res, next) {
+// server.get('/tokens/:sessionName', function (req, res, next) {
+//
+//     if (!sessions[req.params.sessionName]) {
+//         rtcat.createSession({label: req.params.sessionName}, function (err, resp) {
+//
+//             if (err) return res.json(400, {error: err.message});
+//             sessions[req.params.sessionName] = resp.uuid;
+//
+//             var opts = Object.assign({},
+//                 {session_id: sessions[req.params.sessionName]},
+//                 req.query);
+//
+//             rtcat.createToken(opts, function (err, resp) {
+//                 if (err) {
+//                     return res.json(400, {error: err.message})
+//                 }
+//                 return res.json(200, resp)
+//             });
+//         });
+//     }
+//
+//     var opts = Object.assign({},
+//         {session_id: sessions[req.params.sessionName]},
+//         req.query);
+//
+//     rtcat.createToken(opts, function (err, resp) {
+//         if (err) {
+//             return res.json(400, {error: err.message})
+//         }
+//         return res.json(200, resp)
+//     });
+//
+// });
 
+server.get('/tokens/:sessionName', sessionMiddleware, tokenMiddleware);
+
+function sessionMiddleware(req, res, next) {
     if (!sessions[req.params.sessionName]) {
         rtcat.createSession({label: req.params.sessionName}, function (err, resp) {
             if (err) return res.json(400, {error: err.message});
             sessions[req.params.sessionName] = resp.uuid;
-            var opts = {
-                session_id: sessions[req.params.sessionName]
-            };
-
-            rtcat.createToken(opts, function (err, resp) {
-                if (err) {
-                    return res.json(400, {error: err.message})
-                }
-                return res.json(200, resp)
-            });
+            next();
         });
+    } else {
+        next();
     }
-
-    var opts = {
-        session_id: sessions[req.params.sessionName]
-    };
-
-    rtcat.createToken(opts, function (err, resp) {
-        if (err) {
-            return res.json(400, {error: err.message})
-        }
-        return res.json(200, resp)
-    });
-
-});
-
-server.get('/sessions', function (req, res, next) {
-
-    rtcat.createSession({label: 'demo'}, function (err, resp) {
-        if (err) {
-            return res.json(400, {error: err.message})
-        }
-        return res.json(200, resp)
-    });
-
-});
-
-server.get('/:sessionId/tokens', function (req, res, next) {
-
-    var opts = {
-        session_id: req.params.sessionId
-    };
-
-    rtcat.createToken(opts, function (err, resp) {
-        if (err) {
-            return res.json(400, {error: err.message})
-        }
-        return res.json(200, resp)
-    });
-
-});
-
-// ################### Android 1v1 demo backend ####################### //
-
-function MyEventHandler() {
-    EventEmitter.call(this);
 }
 
-util.inherits(MyEventHandler, EventEmitter);
+function tokenMiddleware(req, res, next) {
+    var opts = Object.assign({},
+        {session_id: sessions[req.params.sessionName]},
+        req.query);
 
-var sockets = [];
-var myEvent = new MyEventHandler();
-
-myEvent.on("in", function (data, soc) {
-
-    console.log("here are " + sockets.length);
-
-    if (sockets.length > 0) {
-        var cp_soc = sockets[0];
-        sockets.shift();
-        rtcat.createSession({label: 'demo'}, function (err, resp) {
-
-            var sessionId = resp.uuid;
-            var json = {eventName: "get_token"};
-            var opts = {
-                session_id: sessionId
-            };
-
-            rtcat.createToken(opts, function (err, resp) {
-                var token = resp.uuid;
-                console.log(token);
-                json.token = token;
-                cp_soc.emit("new message", json);
-            });
-
-            rtcat.createToken(opts, function (err, resp) {
-                var token = resp.uuid;
-                console.log(token);
-                json.token = token;
-                soc.emit("new message", json);
-            });
-
-
-        });
-
-    } else {
-        var json = {eventName: "waiting"};
-        soc.emit("new message", json);
-        sockets.push(soc);
-    }
-
-});
-
-io.on('connection', function (socket) {
-
-    socket.id = uuid.v4();
-
-    socket.on("new message", function (message) {
-        var json = JSON.parse(message);
-        var eventName = json.eventName;
-        var content = json.content;
-        console.log(eventName, content);
-        myEvent.emit(eventName, content, socket);
+    rtcat.createToken(opts, function (err, resp) {
+        if (err) {
+            return res.json(400, {error: err.message})
+        }
+        return res.json(200, resp)
     });
-
-    socket.on('disconnect', function () {
-        arrayRemove(sockets, socket);
-        console.log('a user leave');
-    });
-
-    console.log('a user connected');
-
-});
+}
 
 server.listen(port, function () {
-    console.log('socket.io server listening at %s', server.url);
+    console.log('rtcat-demo-web backend server listening at %s', server.url);
 });
 
 /**
@@ -171,12 +97,5 @@ function normalizePort(val) {
     }
 
     return false;
-}
-
-function arrayRemove(array, value) {
-    var index = array.indexOf(value);
-    if (index > -1) {
-        array.splice(index, 1);
-    }
 }
 
